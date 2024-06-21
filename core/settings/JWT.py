31f329a43b8bfb62d268cfd954ffb2d2b75c0f4b
romazang <zr12169@auca.kg>
2024-06-21 12:41:40 +0600

    added req.txt simple jwt and decouple

--- core/settings/JWT.py
+++ core/settings/JWT.py
@@ -3,13 +3,14 @@ from decouple import config
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=int(config('ACCESS_TOKEN_LIFETIME'))),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=int(config('REFRESH_TOKEN_LIFETIME'))),
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(days=int(config('ACCESS_TOKEN_LIFETIME'))),
+    # 'REFRESH_TOKEN_LIFETIME': timedelta(days=int(config('REFRESH_TOKEN_LIFETIME'))),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=90),
     # чтобы при получении нового access токена, refresh тоже менялся
     'ROTATE_REFRESH_TOKENS': True,
     # чтобы уже существовавшие refresh токены снова не генерились
     'BLACKLIST_AFTER_ROTATION': True,
-
 
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
